929dff599646ac00aab7134d1548e3853de4bb45
morioka1206 <r.morioka@icloud.com>
2021-06-29 17:52:29 +0900

    動画のサイズやアウトライン。トップページリンクを貼った

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get 'wines/top'
   get 'wines/howto'
 
   resources :wines do
